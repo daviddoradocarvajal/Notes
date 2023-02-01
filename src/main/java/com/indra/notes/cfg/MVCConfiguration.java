b5ba8897148988ee5b62107a8cc61be1e0633fe9
akhalilov <khlvamir@gmail.com>
2023-02-01 12:57:28 +0100

    Return JSON

--- src/main/java/com/indra/notes/cfg/MVCConfiguration.java
+++ src/main/java/com/indra/notes/cfg/MVCConfiguration.java
@@ -1,14 +1,22 @@
 package com.indra.notes.cfg;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
+@ComponentScan(basePackages  = "com.indra.notes.controller")
+@Configuration
 public class MVCConfiguration {	
 	
 	@Bean
 	public ViewResolver viewResolver() {		
-		InternalResourceViewResolver vr = new InternalResourceViewResolver();		
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setPrefix("/view/");
+		vr.setSuffix(".jsp");
+		vr.setViewClass(JstlView.class);
 		return vr;
 	}
 }
--- src/main/java/com/indra/notes/controller/NotesController.java
+++ src/main/java/com/indra/notes/controller/NotesController.java
@@ -1,10 +1,20 @@
 package com.indra.notes.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.indra.notes.entity.Note;
 import com.indra.notes.model.GestorNote;
 import com.indra.notes.persistence.NoteRepository;
 
@@ -13,13 +23,38 @@ public class NotesController {
 	
 	@Autowired
 	GestorNote noteService;
+	
 	@Autowired
 	NoteRepository noteRepository;
 	
-	@GetMapping("/listNotes")
-	public ModelAndView listNotes() {
-		ModelAndView mav = new ModelAndView();
-		return mav;		
+	public NotesController() {
+		System.out.println("Creando controller");
+	}
+
+	
+	@GetMapping(path ="/listNotes", headers = "Accept=*/*", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.ALL_VALUE)
+	public ResponseEntity<?> listNotes() {
+		System.out.println("POR AQUI");
+		var note = new Note();
+		
+		note.setId(1);
+		note.setDescripcion("test");
+		note.setIsFavorite(true);
+		note.setTitulo("dasdas");
+		note.setTimestamp(LocalDateTime.now());
+	
+		String jsonStr = "";
+		
+		try {
+			ObjectMapper obj = new ObjectMapper();
+			obj.registerModule(new JavaTimeModule());
+			 jsonStr = obj.writeValueAsString(note);
+			
+		}catch(IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		return ResponseEntity.ok(jsonStr);
 		
 	}
-}
+} 
